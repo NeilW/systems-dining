55defbeb46b8ca9519bafb46471a2f0359a28f2d
Neil Wilson <neil@aldur.co.uk>
2023-12-12 09:35:49 +0000

    Revert "Restrict user capabilities"
    This reverts commit 9802c0fdabf6dba81e4e386c1ee236e9237c2dd6.

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ eval ${APT_GET} install sendmail mailutils systemd-container finger inotify-tool
 echo "Installing simulation"
 install bin/* /usr/local/bin
 install sbin/* /usr/local/sbin
-cp -r system/* /etc/systemd/system
 cp -r user/* /etc/systemd/user
 install -m 644 skel/.forward -T /etc/skel/.forward
 mkdir -p /etc/skel/.config/systemd/user/default.target.wants
