a9e86bbbfb0186adfd31da19a799e88048151b86
Neil Wilson <neil@aldur.co.uk>
2023-12-20 13:09:40 +0000

    Add TLS login to setup script
    Extend the machinectl login mechanism over the network

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Setting up philosophers container"
 
-apt-get install -y systemd-container
+DEBIAN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt-get install -y systemd-container socat
 machinectl pull-tar --verify=checksum https://github.com/NeilW/systemd-dining/releases/download/latest/philosophers.tar.xz
 mkdir -p /etc/systemd/nspawn /etc/systemd/network/80-container-ve.network.d
 cp /var/lib/machines/philosophers.nspawn /etc/systemd/nspawn/philosophers.nspawn
@@ -11,3 +11,46 @@ cat > /etc/systemd/network/80-container-ve.network.d/ipv6prefix.conf <<-END
 Prefix=fd00:$(hexdump -v -n2 -e' /1 "%02x"' /dev/urandom)::/64
 Assign=true
 END
+
+echo "Setting up login units"
+
+mkdir -p /etc/sysusers.d
+cat > /etc/sysusers.d/tlssh.conf <<-USERS
+g gatekeeper - -
+u tlssh - "Philosophers secure login" /nonexistent
+m tlssh gatekeeper
+USERS
+systemd-sysusers
+cat > /etc/polkit-1/rules.d/10-gatekeeper-login.rules <<-POLKIT
+polkit.addRule(function(action, subject) {
+    if (action.id == "org.freedesktop.machine1.login" &&
+        subject.isInGroup("gatekeeper")) {
+        return polkit.Result.YES;
+    }
+});
+POLKIT
+cat > /etc/systemd/system/tlssh.service <<-UNIT
+[Unit]
+Description=TLS secure login server
+After=network.target auditd.service
+
+[Service]
+Restart=on-failure
+ExecStart=/usr/bin/socat TCP6-L:8000,fork,reuseaddr EXEC:"/usr/bin/machinectl login philosophers",pty,ctty,setsid,stderr
+SuccessExitStatus=143
+DynamicUser=yes
+User=tlssh
+Group=gatekeeper
+KillMode=process
+RestrictAddressFamilies=AF_INET AF_INET6 AF_UNIX
+ProtectHostname=yes
+ProtectClock=yes
+ProtectKernelTunables=yes
+ProtectKernelModules=yes
+ProtectKernelLogs=yes
+UMask=077
+
+[Install]
+WantedBy=multi-user.target
+UNIT
+systemctl enable --now tlssh
