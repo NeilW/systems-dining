0b14329dc2fbdbf6053a9b318ce52e99e22b65b3
Neil Wilson <neil@aldur.co.uk>
2023-12-29 12:32:15 +0000

    Add a Websocket terminal
    Set up a websocket interface using 'websocat' and serve a terminal
    page that connects to the TLS interface over a websocket

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Setting up philosophers container"
 
-DEBIAN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt-get install -y systemd-container socat
+DEBIAN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt-get install -y systemd-container
 machinectl pull-tar --verify=checksum https://github.com/NeilW/systemd-dining/releases/download/latest/philosophers.tar.xz
 mkdir -p /etc/systemd/nspawn /etc/systemd/network/80-container-ve.network.d
 [ ! -e /var/lib/machines/philosophers.nspawn ] || cp /var/lib/machines/philosophers.nspawn /etc/systemd/nspawn/philosophers.nspawn
@@ -11,6 +11,14 @@ cat > /etc/systemd/network/80-container-ve.network.d/ipv6prefix.conf <<-END
 Prefix=fd00:$(hexdump -v -n2 -e' /1 "%02x"' /dev/urandom)::/64
 Assign=true
 END
+
+echo "Installing terminal routing software"
+DEBIAN_FRONTEND=noninteractive NEEDRESTART_SUSPEND=1 apt-get install -y socat
+if [ ! -e /usr/local/bin/websocat ]
+then
+    curl -L -o /usr/local/bin/websocat https://github.com/vi/websocat/releases/download/v1.12.0/websocat_max.x86_64-unknown-linux-musl
+    chmod a+x /usr/local/bin/websocat
+fi
 
 echo "Setting up login units"
 
@@ -29,6 +37,16 @@ polkit.addRule(function(action, subject) {
     }
 });
 POLKIT
+fqdn=$(hostname -f)
+terminal_dir=/usr/local/share/terminal
+snap install certbot
+certbot certonly -q -n --register-unsafely-without-email --expand --agree-tos --standalone -d "ipv6.${fqdn}"
+chgrp -R gatekeeper /etc/letsencrypt/live /etc/letsencrypt/archive
+chmod -R g+r /etc/letsencrypt/live /etc/letsencrypt/archive
+chmod g+x /etc/letsencrypt/live /etc/letsencrypt/archive
+
+mkdir -p "${terminal_dir}"
+curl -L -o "${terminal_dir}/index.html" https://raw.githubusercontent.com/newwayland/terminal/v0.0.1/index.html
 cat > /etc/systemd/system/tlssh.service <<-UNIT
 [Unit]
 Description=TLS secure login server
@@ -36,7 +54,7 @@ After=network.target auditd.service
 
 [Service]
 Restart=on-failure
-ExecStart=/usr/bin/socat -T 300 OPENSSL-LISTEN:8000,pf=ip6,fork,reuseaddr,verify=0,cert=/etc/letsencrypt/live/ipv6.$(hostname -f)/fullchain.pem,key=/etc/letsencrypt/live/ipv6.$(hostname -f)/privkey.pem EXEC:"/usr/bin/machinectl login philosophers",pty,ctty,setsid,stderr
+ExecStart=/usr/bin/socat -T 300 OPENSSL-LISTEN:8000,pf=ip6,fork,reuseaddr,verify=0,cert=/etc/letsencrypt/live/ipv6.${fqdn}/fullchain.pem,key=/etc/letsencrypt/live/ipv6.${fqdn}/privkey.pem EXEC:"/usr/bin/machinectl login philosophers",pty,ctty,setsid,stderr
 SuccessExitStatus=143
 DynamicUser=yes
 User=tlssh
@@ -53,9 +71,30 @@ UMask=077
 [Install]
 WantedBy=multi-user.target
 UNIT
-snap install certbot
-certbot certonly -q -n --register-unsafely-without-email --expand --agree-tos --standalone -d "ipv6.$(hostname -f)"
-chgrp -R gatekeeper /etc/letsencrypt/live /etc/letsencrypt/archive
-chmod -R g+r /etc/letsencrypt/live /etc/letsencrypt/archive
-chmod g+x /etc/letsencrypt/live /etc/letsencrypt/archive
-systemctl enable --now tlssh
+cat > /etc/systemd/system/wsssh.service <<-WSS
+[Unit]
+Description=WSS secure login server
+After=network.target auditd.service tlssh.service
+Wants=tlssh.service
+
+[Service]
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+Restart=on-failure
+ExecStart=/usr/bin/socat -T 300 OPENSSL-LISTEN:443,pf=ip6,fork,reuseaddr,verify=0,cert=/etc/letsencrypt/live/ipv6.${fqdn}/fullchain.pem,key=/etc/letsencrypt/live/ipv6.${fqdn}/privkey.pem EXEC:'/usr/local/bin/websocat -E --tls-domain ipv6.${fqdn} --restrict-uri=/ws -F=/\\\\:text/html\\\\:${terminal_dir}/index.html --binary ws-u\\\\:asyncstdio\\\\: ssl\\\\:tcp\\\\:[::]\\\\:8000'
+SuccessExitStatus=143
+DynamicUser=yes
+User=tlssh
+Group=gatekeeper
+KillMode=process
+RestrictAddressFamilies=AF_INET AF_INET6 AF_UNIX
+ProtectHostname=yes
+ProtectClock=yes
+ProtectKernelTunables=yes
+ProtectKernelModules=yes
+ProtectKernelLogs=yes
+UMask=077
+
+[Install]
+WantedBy=multi-user.target
+WSS
+systemctl enable --now wsssh.service tlssh.service
